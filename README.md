43db943d2fd2ffba52ef1d75be431f7847a7bc2f
Jayce Ese <jayce.ese@gmail.com>
2023-02-26 22:17:51 +1000

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Building Responsive Modern React Website
-
-Built React website following JavaScript Mastery course using their Figma Design.
-
-<img width="2559" alt="gpt3-je-screenshot" src="https://user-images.githubusercontent.com/94608894/221408508-0835dd4f-07f8-4595-bbed-e3e3886b8f6f.png">
-
-Figma Design here: 
-https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3?node-id=0%3A1&t=KPo6AEGwMmz9uMsG-0
---
+++ precache-manifest.cf8495ef04d32969527539f30c06879e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7616a32e2ce93648e4b4",
+    "url": "/gpt3_je/static/css/main.acff43ca.chunk.css"
+  },
+  {
+    "revision": "7616a32e2ce93648e4b4",
+    "url": "/gpt3_je/static/js/main.7616a32e.chunk.js"
+  },
+  {
+    "revision": "4824bc4903f26c2d73f7",
+    "url": "/gpt3_je/static/js/1.4824bc49.chunk.js"
+  },
+  {
+    "revision": "41372e5c23c39fa5c272",
+    "url": "/gpt3_je/static/js/runtime~main.41372e5c.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/gpt3_je/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/gpt3_je/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/gpt3_je/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/gpt3_je/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/gpt3_je/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/gpt3_je/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/gpt3_je/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/gpt3_je/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/gpt3_je/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "81547937a09add03b82fa93fe1fb116c",
+    "url": "/gpt3_je/index.html"
+  }
+];
