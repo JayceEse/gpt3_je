3cdab75f48594a49aa209ea45be1ce85e66f8022
Jayce Ese <jayce.ese@gmail.com>
2023-05-16 22:21:44 +1000

    add animation to header

--- src/componets/brand/Brand.jsx
+++ src/componets/brand/Brand.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import { google, slack, atlassian, dropbox, shopify} from "./imports";
-import './brand.css';
+import React from "react";
+import { motion } from "framer-motion/dist/framer-motion";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+import "./brand.css";
 
 const Brand = () => {
   return (
-    <div className='gpt3__brand section__padding'>
-        <div>
-          <img src={google} alt="google" />
-        </div>
-        <div>
-          <img src={slack} alt="slack" />
-        </div>
-        <div>
-         <img src={atlassian} alt="atlassian" />
-        </div>
-        <div>
-          <img src={dropbox} alt="dropbox" />
-        </div>
-        <div>
-          <img src={shopify} alt="shopify" />
-        </div>
+    <div className="gpt3__brand section__padding">
+      <motion.div
+        initial={{ x: "-100vw" }}
+        whileInView={{ x: 0 }}
+        transition={{ type: "tween", duration: 1 }}
+      >
+        <img src={google} alt="google" />
+      </motion.div>
+      <motion.div>
+        <img src={slack} alt="slack" />
+      </motion.div>
+      <motion.div>
+        <img src={atlassian} alt="atlassian" />
+      </motion.div>
+      <motion.div>
+        <img src={dropbox} alt="dropbox" />
+      </motion.div>
+      <motion.div>
+        <img src={shopify} alt="shopify" />
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import people from '../../assets/people.png';
-import ai from '../../assets/ai.png';
-import './header.css';
+import React from "react";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+import { motion } from "framer-motion/dist/framer-motion";
+import "./header.css";
 
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">
+        Let&apos;s Build Something amazing with GPT-3 OpenAI
+      </h1>
+      <p>
+        Yet bed any for travelling assistance indulgence unpleasing. Not
+        thoughts all exercise blessing. Indulgence way everything joy alteration
+        boisterous the attachment. Party we years to order allow asked of.
+      </p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
@@ -20,9 +27,15 @@ const Header = () => (
       </div>
     </div>
 
-    <div className="gpt3__header-image">
+    <motion.div
+      initial={{ y: 25, opacity: 1 }}
+      whileInView={{ opacity: 1 }}
+      animate={{ y: [-25, 25, -25, 25, -25] }}
+      transition={{ repeat: Infinity, duration: 6 }}
+      className="gpt3__header-image"
+    >
       <img src={ai} alt="ai" />
-    </div>
+    </motion.div>
   </div>
 );
 
